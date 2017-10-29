548fac9acbaf3b4ac6f0b445ec2a3f2340aaaff5
Shaun-Roberts <syhrsyhr@gmail.com>
2017-10-29 20:05:14 +1300

    Add files via upload

---
+++ ClosenessSD.R
@@ -0,0 +1,116 @@
+closeness.SD = function(ylimits = c(0,15), size = 30, step_size = 5){
+  data = c("sampson", "kapferer", "samplk", "faux.mesa.high")
+  lincol = c("#D7191C", "#FDAE61", "#ABD9E9", "#c351e2")
+  linewidth = 2
+  
+  
+  ydiffs = diff(ylimits)
+  yupper = ylimits[2]
+  ylower = ylimits[1]
+  
+  
+  plot.new()
+  plot.window(xlim = c(0,100), ylim = c(0,3*ydiffs), xaxs = "i", yaxs = "i")
+  #Axes
+  {
+    axis(1,
+         at = seq(0,100, 10),
+         labels = c(seq(0, 40, 10), seq(0, 50, 10)),
+         las = 1
+    )
+    axis(2,
+         at = seq(0,ydiffs, step_size),
+         labels = seq(ylower, yupper, step_size),
+         las = 1
+    )
+    axis(2,
+         at = seq(0,ydiffs, step_size) + 2*ydiffs,
+         labels = seq(ylower, yupper, step_size),
+         las = 1
+         
+    )
+    axis(4,
+         at = seq(0,ydiffs, step_size) + ydiffs,
+         labels = seq(ylower, yupper, step_size),
+         las = 1
+    )
+  }
+  # Horizontal dividing lines:
+  abline(h = c(ydiffs, 2*ydiffs), lwd = 1)
+  #Titles
+  title(xlab = "False Negative Rate", 
+        ylab = "Standard Deviation", 
+        main = paste("Closeness Centrality Standard Deviation\nNetwork Size (", size, ")", sep = ""))
+  #Plotted lines
+  {
+    # f.pos.rate 0
+    {
+      f.pos.rate = 0
+      for(i in 1:4){
+        load(paste(data[i], size, f.pos.rate,0.01, sep = "_"))
+        lines(x = 0:50, y = unlist(lapply(record$closeness.rec, sd)) + 2*ydiffs - ylower, type = "l", col = lincol[i], lwd = linewidth)
+      }
+    }
+    # f.pos.rate 0.01
+    {f.pos.rate = 0.01
+      for(i in 1:4){
+        load(paste(data[i], size, f.pos.rate,0.01, sep = "_"))
+        lines(x = 0:50 + 50, y = unlist(lapply(record$closeness.rec, sd)) + 2*ydiffs - ylower, type = "l", col = lincol[i], lwd = linewidth)
+      }
+    }
+    # f.pos.rate 0.05
+    {f.pos.rate = 0.05
+      for(i in 1:4){
+        load(paste(data[i], size, f.pos.rate,0.01, sep = "_"))
+        lines(x = 0:50, y = unlist(lapply(record$closeness.rec, sd)) + ydiffs - ylower, type = "l", col = lincol[i], lwd = linewidth)
+      }
+    }
+    # f.pos.rate 0.10
+    {f.pos.rate = 0.10
+      for(i in 1:4){
+        load(paste(data[i], size, f.pos.rate,0.01, sep = "_"))
+        lines(x = 0:50 + 50, y = unlist(lapply(record$closeness.rec, sd)) + ydiffs - ylower, type = "l", col = lincol[i], lwd = linewidth)
+      }
+    }
+    # f.pos.rate 0.15
+    {
+      f.pos.rate = 0.15
+      for(i in 1:4){
+        load(paste(data[i], size, f.pos.rate,0.01, sep = "_"))
+        lines(x = 0:50, y = unlist(lapply(record$closeness.rec, sd)) - ylower, type = "l", col = lincol[i], lwd = linewidth)
+      }
+    }
+    # f.pos.rate 0.20
+    {
+      f.pos.rate = 0.20
+      for(i in 1:4){
+        load(paste(data[i], size, f.pos.rate,0.01, sep = "_"))
+        lines(x = 0:50 + 50, y = unlist(lapply(record$closeness.rec, sd)) - ylower, type = "l", col = lincol[i], lwd = linewidth)
+      }
+    }
+  }
+  
+  #Vertical dividing lines and box
+  abline(v = 50, lwd = 2)
+  box(lwd = 1)
+}
+#30
+pdf("closeness_SD_30.pdf")
+closeness.SD(ylimits = c(0,0.003), size = 30, step_size = 0.001)
+dev.off()
+#100
+pdf("closeness_SD_100.pdf")
+closeness.SD(ylimits = c(0,0.0004), size = 100, step_size = 0.0001)
+dev.off()
+#250
+pdf("closeness_SD_250.pdf")
+closeness.SD(ylimits = c(0,0.0001), size = 250, step_size = 0.00002)
+dev.off()
+#500
+pdf("closeness_SD_500.pdf")
+closeness.SD(ylimits = c(0,0.00004), size = 500, step_size = 0.00001)
+dev.off()
+#1000
+pdf("closeness_SD_1000.pdf")
+closeness.SD(ylimits = c(0,0.00002), size = 1000, step_size = 0.000005)
+dev.off()
---
+++ InformationSD.R
@@ -0,0 +1,129 @@
+info.SD = function(ylimits = c(0,0.8), size = 30, step_size = 0.1){
+  data = c("sampson", "kapferer", "samplk", "faux.mesa.high")
+  lincol = c("#D7191C", "#FDAE61", "#ABD9E9", "#c351e2")
+  linewidth = 2
+  
+  
+  ydiffs = diff(ylimits)
+  yupper = ylimits[2]
+  ylower = ylimits[1]
+  
+  
+  plot.new()
+  plot.window(xlim = c(0,100), ylim = c(0,3*ydiffs), xaxs = "i", yaxs = "i")
+  #Axes
+  {
+    axis(1,
+         at = seq(0,100, 10),
+         labels = c(seq(0, 40, 10), seq(0, 50, 10)),
+         las = 1
+    )
+    axis(2,
+         at = seq(0,ydiffs, step_size),
+         labels = seq(ylower, yupper, step_size),
+         las = 1
+    )
+    axis(2,
+         at = seq(0,ydiffs, step_size) + 2*ydiffs,
+         labels = seq(ylower, yupper, step_size),
+         las = 1
+    )
+    axis(3,
+         at = 50,
+         labels = "",
+         lwd = 2
+    )
+    axis(1,
+         at = 50,
+         labels = "",
+         lwd = 2
+    )
+    axis(4,
+         at = seq(0,ydiffs, step_size) + ydiffs,
+         labels = seq(ylower, yupper, step_size),
+         las = 1
+    )
+  }
+  # Horizontal dividing lines:
+  abline(h = c(ydiffs, 2*ydiffs), lwd = 1)
+  #Titles
+  title(xlab = "False Negative Rate", 
+        ylab = "Standard Deviation", 
+        main = paste("Information Centrality Standard Deviation\nNetwork Size (", size, ")", sep = ""))
+  #Plotted lines
+  {
+    # f.pos.rate 0
+    {
+      f.pos.rate = 0
+      for(i in 1:4){
+        load(paste(data[i], size, f.pos.rate,0.01, sep = "_"))
+        lines(x = 0:50, y = unlist(lapply(record$info.cent.rec, sd)) + 2*ydiffs  - ylower, type = "l", col = lincol[i], lwd = linewidth)
+      }
+    }
+    # f.pos.rate 0.01
+    {f.pos.rate = 0.01
+      for(i in 1:4){
+        load(paste(data[i], size, f.pos.rate,0.01, sep = "_"))
+        lines(x = 0:50 + 50, y = unlist(lapply(record$info.cent.rec, sd)) + 2*ydiffs - ylower, type = "l", col = lincol[i], lwd = linewidth)
+      }
+    }
+    # f.pos.rate 0.05
+    {f.pos.rate = 0.05
+      for(i in 1:4){
+        load(paste(data[i], size, f.pos.rate,0.01, sep = "_"))
+        lines(x = 0:50, y = unlist(lapply(record$info.cent.rec, sd)) + ydiffs - ylower, type = "l", col = lincol[i], lwd = linewidth)
+      }
+    }
+    # f.pos.rate 0.10
+    {f.pos.rate = 0.10
+      for(i in 1:4){
+        load(paste(data[i], size, f.pos.rate,0.01, sep = "_"))
+        lines(x = 0:50 + 50, y = unlist(lapply(record$info.cent.rec, sd)) + ydiffs - ylower, type = "l", col = lincol[i], lwd = linewidth)
+      }
+    }
+    # f.pos.rate 0.15
+    {
+      f.pos.rate = 0.15
+      for(i in 1:4){
+        load(paste(data[i], size, f.pos.rate,0.01, sep = "_"))
+        lines(x = 0:50, y = unlist(lapply(record$info.cent.rec, sd)) - ylower, type = "l", col = lincol[i], lwd = linewidth)
+      }
+    }
+    # f.pos.rate 0.20
+    {
+      f.pos.rate = 0.20
+      for(i in 1:4){
+        load(paste(data[i], size, f.pos.rate,0.01, sep = "_"))
+        lines(x = 0:50 + 50, y = unlist(lapply(record$info.cent.rec, sd)) - ylower, type = "l", col = lincol[i], lwd = linewidth)
+      }
+    }
+  }
+  
+  #Vertical dividing lines and box
+  abline(v = 50, lwd = 2)
+  box(lwd = 1)
+  
+}
+#30
+pdf("info_sd_30_nofun.pdf")
+info.SD(ylimits = c(0,0.8), size = 30, step_size = 0.2)
+dev.off()
+pdf("info_sd_30_fun.pdf")
+info.SD(ylimits = c(0,8), size = 30, step_size = 1)
+dev.off()
+#100
+pdf("info_sd_100.pdf")
+info.SD(ylimits = c(0,1.2), size = 100, step_size = 0.2)
+dev.off()
+#250
+pdf("info_sd_250.pdf")
+info.SD(ylimits = c(0,1.8), size = 250, step_size = 0.4)
+dev.off()
+#500
+pdf("info_sd_500.pdf")
+info.SD(ylimits = c(0,2.5), size = 500, step_size = 0.5)
+dev.off()
+#1000
+pdf("info_sd_1000.pdf")
+info.SD(ylimits = c(0,3.5), size = 1000, step_size = 0.5)
+dev.off()
